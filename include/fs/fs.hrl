63f0a67ff94c30eabe0724129e7ae0ec8f7b014b
Namdak Tonpa <maxim@synrc.com>
2019-05-04 01:58:52 +0300

    fix dialyzer

--- include/fs/fs.hrl
+++ include/fs/fs.hrl
@@ -3,7 +3,11 @@
 
 -include_lib("kvs/include/kvs.hrl").
 
--record(folder, { ?CONTAINER,        description=[], link=[] }).
+-record(folder, {  id=[] :: term(),
+                   top=[] :: [] | integer(),
+                   rear=[] :: [] | integer(),
+                   count=0 :: integer(),
+                   description=[], link=[] }).
 -record(i,      { ?ITERATOR(folder), type=[], ref=[] }).
 
 -endif.
